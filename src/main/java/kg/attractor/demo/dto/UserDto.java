3c365362f780aab721303d2fcb59a3e7143b15a3
Askar <nurbekovaskar099@gmail.com>
2025-07-15 13:29:06 +0600

    Created class dto for users

---
+++ src/main/java/kg/attractor/demo/dto/UserDto.java
@@ -0,0 +1,11 @@
+package kg.attractor.demo.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private String name;
+    private String email;
+    private String password;
+    private String accountType;
+}
