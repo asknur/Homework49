1252c82714ca1c4c2ceff8c098ac191a024fe57f
Askar <nurbekovaskar099@gmail.com>
2025-07-18 18:17:26 +0600

    Added new annotations

--- src/main/java/kg/attractor/demo/model/User.java
+++ src/main/java/kg/attractor/demo/model/User.java
@@ -2,9 +2,12 @@ package kg.attractor.demo.model;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
-@Builder
+@Getter
+@Setter
 public class User {
     private int id;
     private String name;
