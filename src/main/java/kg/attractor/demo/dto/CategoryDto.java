89559d670c4939342b13ba617268f7a8b34ecdb2
Askar <nurbekovaskar099@gmail.com>
2025-08-08 17:19:47 +0600

    Added annotations

--- src/main/java/kg/attractor/demo/dto/CategoryDto.java
+++ src/main/java/kg/attractor/demo/dto/CategoryDto.java
@@ -1,9 +1,15 @@
 package kg.attractor.demo.dto;
 
 import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryDto {
 
     @NotBlank
