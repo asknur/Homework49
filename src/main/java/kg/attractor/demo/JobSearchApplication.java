1e99fc29f6ea618a573d827acaf1bc4e70b52641
Askar <nurbekovaskar099@gmail.com>
2025-08-08 17:19:58 +0600

    Fixes

--- src/main/java/kg/attractor/demo/JobSearchApplication.java
+++ src/main/java/kg/attractor/demo/JobSearchApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MovieReviewApplication {
+public class JobSearchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MovieReviewApplication.class, args);
+		SpringApplication.run(JobSearchApplication.class, args);
 	}
 
 }
