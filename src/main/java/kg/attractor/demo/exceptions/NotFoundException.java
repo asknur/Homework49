a3ff563eabe53bf442f5c27c39f0d06e738df345
Askar <nurbekovaskar099@gmail.com>
2025-08-11 19:05:53 +0600

    Changes from lesson

--- src/main/java/kg/attractor/demo/exceptions/NotFoundException.java
+++ src/main/java/kg/attractor/demo/exceptions/NotFoundException.java
@@ -1,6 +1,8 @@
 package kg.attractor.demo.exceptions;
 
-public class NotFoundException extends RuntimeException {
+import java.util.NoSuchElementException;
+
+public class NotFoundException extends NoSuchElementException {
     public NotFoundException(String prefix) {
         super(prefix + " not found");
     }
