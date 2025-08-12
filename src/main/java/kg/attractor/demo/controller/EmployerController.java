f3fc277d2ca2532c135f33173bde5d8fcd02fdab
Askar <nurbekovaskar099@gmail.com>
2025-08-12 15:40:47 +0600

    Created class controller and add method for matching list of. resumes

---
+++ src/main/java/kg/attractor/demo/controller/EmployerController.java
@@ -0,0 +1,24 @@
+package kg.attractor.demo.controller;
+
+import kg.attractor.demo.service.UserService;
+import kg.attractor.demo.service.impl.ResumeServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employer")
+@RequiredArgsConstructor
+public class EmployerController {
+    private final UserService userService;
+    private final ResumeServiceImpl resumeService;
+
+    @GetMapping("/resumes")
+    public String resumes(Model model) {
+        model.addAttribute("resumes", resumeService.findAll());
+        return "resume";
+    }
+
+}
