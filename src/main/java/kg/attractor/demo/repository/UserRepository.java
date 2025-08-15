fc66ca134f45603198231fec6c145dcdac799d87
Askar <nurbekovaskar099@gmail.com>
2025-08-15 12:09:35 +0600

    Created repositories for entities

---
+++ src/main/java/kg/attractor/demo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package kg.attractor.demo.repository;
+
+import kg.attractor.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+}
---
+++ src/main/java/kg/attractor/demo/repository/WorkExperienceInfoRepository.java
@@ -0,0 +1,9 @@
+package kg.attractor.demo.repository;
+
+import kg.attractor.demo.model.WorkExperienceInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkExperienceInfoRepository extends JpaRepository<WorkExperienceInfo, String> {
+}
